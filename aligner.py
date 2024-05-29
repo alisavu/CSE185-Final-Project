26a256a783560cc251f8ff7a41caaf65dfafbbdf
alisavu <ahvu@ucsd.edu>
2024-05-28 17:00:03 -0700

    update aligner.py and added init file

--- aligner.py
+++ aligner.py
@@ -3,11 +3,11 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(
-        prog="bwa mem",
-        desctription="Command-line script to perform alignments of fastq files"
+        prog="aligner",
+        description="Command-line script to perform alignments of fastq files"
     )
     
-    # required
+    # required  
     parser.add_argument("idxbase", help="index reference genome file", type=str)
     parser.add_argument("in1.fq", help="input fastq file 1", type=str)
     parser.add_argument("in2.fq", help="input fastq file 2", type=str)    
@@ -16,8 +16,9 @@ def main():
     parser.add_argument("-o", "--out", help="Write output to file." \
         "Defualt: stdout", metavar="FILE", type=str, required=False)
     parser.add_argument("-f", "--fasta-ref", \
-        help="faidx indexed reference sequence file")
+        help="faidx indexed reference sequence file", \
+            type=str, metavar="FILE", required=False)
     args = parser.parse_args()
     
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
