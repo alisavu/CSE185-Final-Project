756355dff0f3df8fa1710c63c5624dc81cbcd39a
alisavu <108474074+alisavu@users.noreply.github.com>
2024-05-29 14:16:54 -0700

    Update aligner.py

--- aligner.py
+++ aligner.py
@@ -7,17 +7,20 @@ def main():
         description="Command-line script to perform alignments of fastq files"
     )
     
-    # required  
+    # required arguments
     parser.add_argument("idxbase", help="index reference genome file", type=str)
     parser.add_argument("in1.fq", help="input fastq file 1", type=str)
     parser.add_argument("in2.fq", help="input fastq file 2", type=str)    
     
-    # optional
-    parser.add_argument("-o", "--out", help="Write output to file." \
-        "Defualt: stdout", metavar="FILE", type=str, required=False)
-    parser.add_argument("-f", "--fasta-ref", \
-        help="faidx indexed reference sequence file", \
-            type=str, metavar="FILE", required=False)
+    # optional arguments
+    parser.add_argument("-o", "--out", help="sam file to output results to [stdout]" \
+        "Default: stdout", type=str, metavar="FILE", required=False)
+    parser.add_argument("-A", help="score for a sequence match", metavar="INT",
+                        type=int, required=False)
+    parser.add_argument("-B", help="penalty for a mismatch", metavar="INT",
+                        type=int, required=False)
+    parser.add_argument("-O", help="penalties for deletions and insertions", 
+                        metavar="INT", type=int, required=False)
     args = parser.parse_args()
     
 if __name__ == "__main__":
